52d180d446fa020e0b34d66a9b379070a44aafab
CllaudiaB <buraanna27@gmail.com>
2024-04-17 11:02:28 +0200

    function which checks the number of arguments and whether the marckdown file exists

--- markdown2html.py
+++ markdown2html.py
@@ -6,16 +6,20 @@ import sys
 import os.path
 
 
-def main():
+def check():
     """Check number of arguments and if Markdown file exist"""
     if len(sys.argv) <= 2:
         print('Usage: ./markdown2html.py README.md '
               'README.html', file=sys.stderr)
-        sys.exit(1)
+        return sys.exit(1)
 
-    if os.path.isfile(sys.argv[1]) == False:
-            print(f"Missing {sys.argv[1]}", file=sys.stderr)
-            sys.exit(1)
+    if os.path.isfile(sys.argv[1]) is False:
+        print(f"Missing {sys.argv[1]}", file=sys.stderr)
+        return sys.exit(1)
+
+
+def main():
+    check()
 
 
 if __name__ == "__main__":
